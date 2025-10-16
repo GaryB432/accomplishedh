5a1171a746d6c4e21803fd712bba47281cc26a4c
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2025-10-16 15:00:23 -0500

    chore: add lifespan client test

--- apps/web/src/lib/components/wikidata/Lifespan.svelte.spec.ts
+++ apps/web/src/lib/components/wikidata/Lifespan.svelte.spec.ts
@@ -29,3 +29,54 @@ test("date of birth is in document", async () => {
     .element(screen.getByText("December 31, 1962, POD, CAUSE, MANNER"))
     .toBeInTheDocument();
 });
+
+test("no manner", async () => {
+  const screen = render(Lifespan, {
+    entity: {
+      id: "",
+      type: "item",
+      summary: {
+        claims: {
+          [P.DATE_OF_BIRTH]: ["1957-01-29"],
+          [P.PLACE_OF_BIRTH]: ["POB"],
+          [P.DATE_OF_DEATH]: ["1962-12-31"],
+          [P.PLACE_OF_DEATH]: ["POD"],
+          [P.CAUSE_OF_DEATH]: ["CAUSE"],
+        },
+        fun: false,
+      },
+    },
+  });
+
+  await expect
+    .element(screen.getByText("January 29, 1957, POB"))
+    .toBeInTheDocument();
+  await expect
+    .element(screen.getByText("December 31, 1962, POD, CAUSE,"))
+    .toBeInTheDocument();
+});
+
+test("no cause", async () => {
+  const screen = render(Lifespan, {
+    entity: {
+      id: "",
+      type: "item",
+      summary: {
+        claims: {
+          [P.DATE_OF_BIRTH]: ["1957-01-29"],
+          [P.PLACE_OF_BIRTH]: ["POB"],
+          [P.DATE_OF_DEATH]: ["1962-12-31"],
+          [P.PLACE_OF_DEATH]: ["POD"],
+        },
+        fun: false,
+      },
+    },
+  });
+
+  await expect
+    .element(screen.getByText("January 29, 1957, POB"))
+    .toBeInTheDocument();
+  await expect
+    .element(screen.getByText("December 31, 1962, POD, ,"))
+    .toBeInTheDocument();
+});
