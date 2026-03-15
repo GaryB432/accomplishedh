1d93e8593599c19706a338be9e1a84f3d4830712
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-03-15 18:47:05 -0500

    build(test): update test configuration to use playwright as the browser provider

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -10,10 +11,9 @@ export default defineConfig({
         extends: "./vite.config.ts",
         test: {
           name: "client",
-          environment: "browser",
           browser: {
             enabled: true,
-            provider: "playwright",
+            provider: playwright(),
             instances: [{ browser: "chromium", headless: true }],
           },
           include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
