a53d7f0fe668d1043f94e4ffce66ce4835d788f3
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-03-24 08:45:40 -0500

    refactor: remove unused playwright examples and demo components

--- apps/next-web/tests/human.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('human page has expected div', async ({ page }) => {
-	await page.goto('/human');
-	expect(await page.textContent('article > div')).toBe('//human route works');
-});
