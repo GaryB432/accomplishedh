36ac9031e267cef00152e984948dbdfc2ada9b8a
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-03-25 10:00:52 -0500

    feat: restore testing

--- apps/next-web/vite.config.ts
+++ apps/next-web/vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { playwright } from "@vitest/browser-playwright";
 import devtoolsJson from "vite-plugin-devtools-json";
 import { defineConfig } from "vitest/config";
 
@@ -7,19 +8,19 @@ export default defineConfig({
   test: {
     expect: { requireAssertions: true },
     projects: [
-      // {
-      //   extends: "./vite.config.ts",
-      //   test: {
-      //     name: "client",
-      //     browser: {
-      //       enabled: true,
-      //       provider: playwright(),
-      //       instances: [{ browser: "chromium", headless: true }],
-      //     },
-      //     include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-      //     exclude: ["src/lib/server/**"],
-      //   },
-      // },
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "client",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [{ browser: "chromium", headless: true }],
+          },
+          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+          exclude: ["src/lib/server/**"],
+        },
+      },
 
       {
         extends: "./vite.config.ts",
