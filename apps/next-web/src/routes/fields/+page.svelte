155a294c6cb35a6f12e4e631a9acbe9245e8c3cf
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-04-18 18:08:22 -0500

    feat: implement fields of work loading and display, refactor page structure, and remove obsolete page.ts

--- apps/next-web/src/routes/fields/+page.svelte
+++ apps/next-web/src/routes/fields/+page.svelte
@@ -1,9 +1,68 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
+  import { apiRequestConfig } from "$lib/wikibase/urls";
+  import { fromDictionary } from "$lib/wikibase/utils";
+  import {
+    entities_get_url,
+    type WikibaseResponse,
+  } from "@accomplishedh/wikibase";
+  import { onMount } from "svelte";
 
-    let { data }: PageProps = $props();
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+
+  let fowqs = $derived(
+    [...data.fieldsOfWork.keys()].toSorted(
+      (a, b) => data.fieldsOfWork.get(b)! - data.fieldsOfWork.get(a)!,
+    ),
+  );
+
+  let u = $derived(
+    entities_get_url({ ids: fowqs.slice(0, 50), props: ["labels"] }), // TODO stop silently skipping!
+  );
+
+  let wikibaseResponsePromise = $state<Promise<WikibaseResponse>>();
+
+  onMount(() => {
+    console.log(u);
+    wikibaseResponsePromise = fetch(u, apiRequestConfig).then(
+      (r) => r.json() as Promise<WikibaseResponse>,
+    );
+  });
 </script>
 
-<h1>lottsa fields</h1>
+<main>
+  <div class="container">
+    <h1>Some Fields Of Work Remarks</h1>
 
-{JSON.stringify(data.featureds.length)}
+    {#await wikibaseResponsePromise}
+      hold on tight for fows
+    {:then possiblySuccessful}
+      {@const fows =
+        possiblySuccessful && possiblySuccessful.success === 1
+          ? Object.values(possiblySuccessful.entities ?? {})
+          : []}
+
+      <section>
+        {#each Object.values(fows) as fow (fow.id)}
+          <div>
+            {fow.id}
+          </div>
+          <div>
+            {fromDictionary(fow.labels)}
+          </div>
+          <div>
+            {data.fieldsOfWork.get(fow.id)}
+          </div>
+        {/each}
+      </section>
+    {/await}
+  </div>
+</main>
+
+<style>
+  section {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+</style>
