bed289cba333fadaf94611c177f3c2fe58d83d5b
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-04-18 18:07:17 -0500

    feat: implement EntityDescription and FieldOfWorkIcon components, enhance FeaturedToday and Headshot components, and update page structure

---
+++ apps/next-web/src/routes/human/EntityDescription.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { fromDictionary } from "$lib/wikibase/utils";
+
+  let { subject } = $props();
+  let description = $derived(fromDictionary(subject.descriptions));
+</script>
+
+<article>
+  {description}
+</article>
+
+<style>
+  article {
+    --some-color: rgba(0, 0, 0, 0.2);
+    box-shadow:
+      0 2px 2px 0 var(--some-color),
+      0 12px 22px 0 var(--some-color);
+    outline: 1px solid var(--some-color);
+    overflow: hidden;
+    margin-block: 2rem;
+    padding: 0.5rem;
+  }
+
+  article:hover {
+    animation: tada 1s;
+  }
+
+  @keyframes tada {
+    from {
+      transform: scale3d(1, 1, 1);
+    }
+
+    10%,
+    20% {
+      transform: scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg);
+    }
+
+    30%,
+    50%,
+    70%,
+    90% {
+      transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg);
+    }
+
+    40%,
+    60%,
+    80% {
+      transform: scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg);
+    }
+
+    to {
+      transform: scale3d(1, 1, 1);
+    }
+  }
+</style>
