4f13c6dfd2bb11984b9d1c732e6c4214c5d5ecea
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-04-25 18:40:16 -0500

    feat(graph): stub human and field connections visualization

--- apps/next-web/src/routes/fields/+page.svelte
+++ apps/next-web/src/routes/fields/+page.svelte
@@ -3,18 +3,89 @@
 
   let { data }: PageProps = $props();
 
-  let asdf = $derived(
+  let humanFowConnections = $derived(
     Object.entries(data.fieldsOfWork.people)
-      .map(([human, j]) => {
-        return j.fows.map((fow) => ({ ...fow, human }));
+      .map(([human, h]) => {
+        return h.fows.map((fow) => ({ ...fow, human }));
       })
       .flat(),
   );
-//   $inspect(asdf);
+
+  type Human = {
+    class: "human";
+    name: string;
+  };
+
+  type Field = {
+    class: "field";
+    name: string;
+  };
+
+  type Point = Field | Human;
+
+  let graph = $derived(
+    humanFowConnections.reduce(
+      (a, hfc) => {
+        const hPoint = { class: "human" as const, name: hfc.human };
+        const fowPoint = { class: "field" as const, name: hfc.id };
+        a.points.add(hPoint);
+        a.points.add(fowPoint);
+        a.edges.add({ from: hPoint, to: fowPoint });
+
+        return a;
+      },
+      { edges: new Set(), points: new Set() } as {
+        edges: Set<{ from: Point; to: Point }>;
+        points: Set<Point>;
+      },
+    ),
+  );
 </script>
 
-{JSON.stringify(
-  asdf.map((g) => g.category),
-  undefined,
-  1,
-)}
+<div class="graph">
+  <div class="points">
+    {#each graph.points as point (point.name)}
+      <div
+        class={[
+          "point",
+          { fow: point.class === "field", human: point.class === "human" },
+        ]}
+      >
+        {point.name}
+      </div>
+    {/each}
+  </div>
+  <div class="edges">
+    {#each graph.edges as edge (edge.from.name
+      .concat("!")
+      .concat(edge.to.name))}
+      <div class={["edge"]}>
+        <div>
+          {edge.from.name}
+        </div>
+        <div>
+          {edge.to.name}
+        </div>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .point {
+    border: thin solid lime;
+  }
+  .edges {
+    display: flex;
+  }
+  .edge {
+    background-color: red;
+  }
+
+  .human {
+    border: thin solid blue;
+  }
+  .fow {
+    border: thin solid orange;
+  }
+</style>
