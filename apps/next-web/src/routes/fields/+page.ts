c12a917a5bebc308ee036af3fea7d4497e62671e
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-04-28 08:36:42 -0500

    refactor(load): change load function to synchronous and simplify data processing

--- apps/next-web/src/routes/fields/+page.ts
+++ apps/next-web/src/routes/fields/+page.ts
@@ -3,10 +3,8 @@ import type { PageLoad } from "./$types";
 import fieldsOfWork from "../../data/wikibase-cache/fields-of-work.json";
 import { grabGraphParts } from "./graph";
 
-export const load = (async () => {
-  const f = Object.values(
-    fieldsOfWork.people as Record<string, { fows: object[] }>,
-  )
+export const load = (() => {
+  Object.values(fieldsOfWork.people as Record<string, { fows: object[] }>)
     .map((g) => g.fows.map((f) => ({ ...f, data: {} })))
     .flat();
 
