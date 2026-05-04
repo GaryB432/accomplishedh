c0bc6a279a668ce44ef034be6af83b64ae2fa980
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-05-04 08:23:34 -0500

    refactor: enhance layout options and improve graph styling in +page.svelte

--- apps/next-web/src/routes/fields/+page.svelte
+++ apps/next-web/src/routes/fields/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-  import cytoscape, { type StylesheetJson } from "cytoscape";
+  import cytoscape, {
+    type LayoutOptions,
+    type StylesheetJson,
+  } from "cytoscape";
   import { onMount } from "svelte";
   import type { PageProps } from "./$types";
 
@@ -8,40 +11,105 @@
 
   let cydiv = $state<HTMLDivElement>();
 
+  const layouts: Record<string, LayoutOptions> = {
+    breadthfirst: { name: "breadthfirst" },
+    circle: { name: "circle" },
+    concentric: { name: "concentric" },
+    cose: { name: "cose" },
+    grid: { name: "grid" },
+    none: { name: "null" },
+    preset: { name: "preset" },
+    random: { name: "random" },
+  };
+
+  //       animate?: boolean;
+  //       // duration of animation in ms if enabled
+  //       animationDuration?: number;
+  //       // easing of animation if enabled
+  //       animationEasing?: Css.TransitionTimingFunction;
+  //       /**
+  //        * a function that determines whether the node should be animated.
+  //        * All nodes animated by default on animate enabled.
+  //        * Non-animated nodes are positioned immediately when the layout starts
+  //        */
+  //       animateFilter?(node: NodeSingular, index: number): boolean;
+
+  // Object.values(layouts).forEach((l: AnimatedLayoutOptions)=> ({...l, anim }))
+
+  let selectedLayoutName = $state<string>("none");
+
   const style: StylesheetJson = [
     {
       selector: "node",
       style: {
         shape: "hexagon",
-        "background-color": "red",
+      },
+    },
+
+    {
+      selector: "node.field",
+      style: {
+        backgroundColor: "red",
+        label: "data(label)",
+      },
+    },
+    {
+      selector: "node.person",
+      style: {
         label: "data(id)",
+      },
+    },
+    {
+      selector: "node.cat",
+      style: {
+        backgroundColor: "orange",
+        label: "data(label)",
       },
     },
   ];
 
-  // let cy = $state<cytoscape.Core>();
+  let cy = $state<cytoscape.Core>();
 
   onMount(() => {
-    cytoscape({
+    const newLocal = layouts[selectedLayoutName];
+    cy = cytoscape({
       container: cydiv,
       elements,
       style,
-      layout: { name: "random" },
+      layout: newLocal,
     });
-    // const layout = cy.layout({ name: "cose" });
-    // layout.run();
   });
 </script>
 
-<div id="cy" class="graph" bind:this={cydiv}></div>
+<section class="main">
+  <div id="cy" class="graph" bind:this={cydiv}></div>
+  <div>
+    <div class="buttons">
+      <select
+        name="lsel"
+        bind:value={selectedLayoutName}
+        onchange={() => {
+          cy?.layout(layouts[selectedLayoutName]).run();
+        }}
+      >
+        {#each Object.keys(layouts) as lopts (lopts)}
+          <option value={lopts}>{lopts}</option>
+        {/each}
+      </select>
+    </div>
+  </div>
+</section>
 
 <style>
+  .main {
+    display: grid;
+    grid-template-columns: 1fr 20vw;
+    border: thin solid lime;
+  }
+
   #cy {
     width: 90vw;
     height: 60vh;
-    /* position: absolute; */
-    top: calc(var(--header-h) + 1em);
-    left: 0;
     border: thin solid silver;
   }
 </style>
