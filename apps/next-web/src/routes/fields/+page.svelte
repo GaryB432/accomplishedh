769ee9ef1d7174c11792e6e07e38befcd5f64e28
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-05-04 08:58:45 -0500

    Dag experimentation (#132)
    * refactor: simplify graph data handling and update createElements function
    
    * fix: remove debug log for today's ISO date in load function
    
    * refactor: update createElements function and add new tests for large and small datasets
    
    * refactor: enhance layout options and improve graph styling in +page.svelte

--- apps/next-web/src/routes/fields/+page.svelte
+++ apps/next-web/src/routes/fields/+page.svelte
@@ -1,65 +1,115 @@
 <script lang="ts">
   import cytoscape, {
-    type ElementDefinition,
+    type LayoutOptions,
     type StylesheetJson,
   } from "cytoscape";
   import { onMount } from "svelte";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
-  let { graph } = $derived(data);
+  let { elements } = $derived(data);
 
   let cydiv = $state<HTMLDivElement>();
 
-  // let layout = $state(cytoscape.La)
+  const layouts: Record<string, LayoutOptions> = {
+    breadthfirst: { name: "breadthfirst" },
+    circle: { name: "circle" },
+    concentric: { name: "concentric" },
+    cose: { name: "cose" },
+    grid: { name: "grid" },
+    none: { name: "null" },
+    preset: { name: "preset" },
+    random: { name: "random" },
+  };
 
-  let { nodes, edges } = $derived(graph);
+  //       animate?: boolean;
+  //       // duration of animation in ms if enabled
+  //       animationDuration?: number;
+  //       // easing of animation if enabled
+  //       animationEasing?: Css.TransitionTimingFunction;
+  //       /**
+  //        * a function that determines whether the node should be animated.
+  //        * All nodes animated by default on animate enabled.
+  //        * Non-animated nodes are positioned immediately when the layout starts
+  //        */
+  //       animateFilter?(node: NodeSingular, index: number): boolean;
 
-  let elements: ElementDefinition[] = $derived([
-    ...nodes.map((node) => ({ data: { ...node } })),
-    ...edges.map(([from, to]) => ({
-      data: {
-        id: from.id.concat("|").concat(to.id),
-        source: from.id,
-        target: to.id,
-      },
-    })),
-  ]);
+  // Object.values(layouts).forEach((l: AnimatedLayoutOptions)=> ({...l, anim }))
+
+  let selectedLayoutName = $state<string>("none");
 
   const style: StylesheetJson = [
     {
       selector: "node",
       style: {
         shape: "hexagon",
-        "background-color": "red",
+      },
+    },
+
+    {
+      selector: "node.field",
+      style: {
+        backgroundColor: "red",
+        label: "data(label)",
+      },
+    },
+    {
+      selector: "node.person",
+      style: {
         label: "data(id)",
+      },
+    },
+    {
+      selector: "node.cat",
+      style: {
+        backgroundColor: "orange",
+        label: "data(label)",
       },
     },
   ];
 
-  // let cy = $state<cytoscape.Core>();
+  let cy = $state<cytoscape.Core>();
 
   onMount(() => {
-    cytoscape({
+    const newLocal = layouts[selectedLayoutName];
+    cy = cytoscape({
       container: cydiv,
       elements,
       style,
-      layout: { name: "random" },
+      layout: newLocal,
     });
-    // const layout = cy.layout({ name: "cose" });
-    // layout.run();
   });
 </script>
 
-<div id="cy" class="graph" bind:this={cydiv}></div>
+<section class="main">
+  <div id="cy" class="graph" bind:this={cydiv}></div>
+  <div>
+    <div class="buttons">
+      <select
+        name="lsel"
+        bind:value={selectedLayoutName}
+        onchange={() => {
+          cy?.layout(layouts[selectedLayoutName]).run();
+        }}
+      >
+        {#each Object.keys(layouts) as lopts (lopts)}
+          <option value={lopts}>{lopts}</option>
+        {/each}
+      </select>
+    </div>
+  </div>
+</section>
 
 <style>
+  .main {
+    display: grid;
+    grid-template-columns: 1fr 20vw;
+    border: thin solid lime;
+  }
+
   #cy {
     width: 90vw;
     height: 60vh;
-    /* position: absolute; */
-    top: calc(var(--header-h) + 1em);
-    left: 0;
-    border: thin solid red;
+    border: thin solid silver;
   }
 </style>
--- apps/next-web/src/routes/fields/cytoscape.ts
+++ apps/next-web/src/routes/fields/cytoscape.ts
@@ -4,49 +4,85 @@ import type {
 } from "@accomplishedh/shared/lib/dto.types";
 import type { EdgeDefinition, NodeDefinition } from "cytoscape";
 
-export function greet(name: string): string {
-  return `cytoscape says: hello to ${name}`;
-}
-export function add(a: number, b: number): number {
-  return a + b;
-}
-export const meaning: { life: number } = {
-  life: 42,
-};
 export function createElements(
   recordByPerson: Record<PersonQid, FieldsOfWorkSummaryV1>,
 ): Array<NodeDefinition | EdgeDefinition> {
-  return [
-    {
-      data: { id: "Q333", category: "Science", label: "astronomy" },
-      classes: ["field"],
-    },
-    {
-      data: { id: "Q12483", category: "Science", label: "statistics" },
-      classes: ["field"],
-    },
-    {
-      data: { id: "Q8242", category: "Lit", label: "literature" },
-      classes: ["field"],
-    },
-    {
-      data: { id: "Q4", category: "Science", label: "engineering" },
-      classes: ["field"],
-    },
-    { data: { id: "Q433773" }, classes: ["person"] },
-    { data: { id: "Q364505" }, classes: ["person"] },
-    { data: { id: "Q200397" }, classes: ["person"] },
-    { data: { id: "Q187258" }, classes: ["person"] },
-    { data: { id: "Q81130" }, classes: ["person"] },
-    { data: { id: "Q245355" }, classes: ["person"] },
-    { data: { id: "Q247603" }, classes: ["person"] },
+  const ckeys = new Set<string>();
+  const hkeys = new Set<string>();
+  const fkeys = new Set<string>();
+  const nodes: Array<NodeDefinition | EdgeDefinition> = [];
 
-    { data: { source: "Q333", target: "Q433773", id: "" } },
-    { data: { source: "Q333", target: "Q364505", id: "" } },
-    { data: { source: "Q12483", target: "Q200397", id: "" } },
-    { data: { source: "Q333", target: "Q187258", id: "" } },
-    { data: { source: "Q333", target: "Q81130", id: "" } },
-    { data: { source: "Q8242", target: "Q245355", id: "" } },
-    { data: { source: "Q4", target: "Q247603", id: "" } },
+  for (const [hq, summary] of Object.entries(recordByPerson)) {
+    hkeys.add(hq);
+    nodes.push({ classes: ["person"], data: { id: hq } });
+
+    for (const fow of summary.fows) {
+      if (!fkeys.has(fow.id)) {
+        fkeys.add(fow.id);
+        nodes.push({ classes: ["field"], data: fow });
+      }
+
+      nodes.push(toEdge(hq, fow.id));
+
+      const capitalizedCat = fow.category.toLocaleUpperCase();
+      if (!ckeys.has(capitalizedCat)) {
+        ckeys.add(capitalizedCat);
+        nodes.push({
+          classes: ["cat"],
+          data: { id: capitalizedCat, label: fow.category },
+        });
+      }
+      nodes.push(toEdge(fow.id, capitalizedCat));
+    }
+  }
+
+  return [
+    ...nodes,
+    //   .toSorted((a, b) => {
+    //     const aid = a.data.id ?? "";
+    //     const bid = b.data.id ?? "";
+    //     return aid.localeCompare(bid);
+    //   }
+
+    // ),
   ];
 }
+
+export function toEdge(source: string, target: string): EdgeDefinition {
+  return { data: { source, target, id: source.concat("→").concat(target) } };
+}
+
+const smoll = [
+  {
+    data: { id: "Q333", category: "Science", label: "astronomy" },
+    classes: ["field"],
+  },
+  {
+    data: { id: "Q12483", category: "Science", label: "statistics" },
+    classes: ["field"],
+  },
+  {
+    data: { id: "Q8242", category: "Lit", label: "literature" },
+    classes: ["field"],
+  },
+  {
+    data: { id: "Q4", category: "Science", label: "engineering" },
+    classes: ["field"],
+  },
+  { data: { id: "Q433773" }, classes: ["person"] },
+  { data: { id: "Q364505" }, classes: ["person"] },
+  { data: { id: "Q200397" }, classes: ["person"] },
+  { data: { id: "Q187258" }, classes: ["person"] },
+  { data: { id: "Q81130" }, classes: ["person"] },
+  { data: { id: "Q245355" }, classes: ["person"] },
+  { data: { id: "Q247603" }, classes: ["person"] },
+  { data: { source: "Q333", target: "Q433773", id: "Q333^Q433773" } },
+  { data: { source: "Q333", target: "Q364505", id: "Q333^Q364505" } },
+  {
+    data: { source: "Q12483", target: "Q200397", id: "Q12483^Q200397" },
+  },
+  { data: { source: "Q333", target: "Q187258", id: "Q333^Q187258" } },
+  { data: { source: "Q333", target: "Q81130", id: "Q333^Q81130" } },
+  { data: { source: "Q8242", target: "Q245355", id: "Q8242^Q245355" } },
+  { data: { source: "Q4", target: "Q247603", id: "Q4^Q247603" } },
+];
