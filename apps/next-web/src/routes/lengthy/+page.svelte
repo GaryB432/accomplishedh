775b656330c3e93ada12e95cc29294e815add2ef
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-05-16 08:24:11 -0500

    feat: add stub component and associated tests

---
+++ apps/next-web/src/routes/lengthy/+page.svelte
@@ -0,0 +1,30 @@
+<section>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quam,
+    reiciendis molestias architecto nisi enim sapiente accusantium eveniet esse,
+    numquam iure distinctio corrupti totam beatae? Nobis assumenda placeat quam
+    repudiandae.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, aliquid. Ut
+    iusto architecto ullam minima, magnam totam deserunt nihil doloribus rerum
+    eligendi sed perspiciatis nesciunt, eum, vero quibusdam fugit odio.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus molestias
+    aut illo eligendi, officiis reprehenderit at autem quo natus aperiam nisi
+    hic non labore a aliquid nemo? Natus, hic aut!
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus rem
+    aperiam magnam assumenda consequuntur. At, nobis. Aliquid ducimus earum,
+    illum sint temporibus voluptatum! Laborum ipsum excepturi, in voluptatem
+    repudiandae impedit?
+  </p>
+</section>
+
+<style>
+  p {
+    margin: 6em 0;
+  }
+</style>
