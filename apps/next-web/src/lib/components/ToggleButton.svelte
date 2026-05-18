5abf1558ac64f22d05afffa82460680d2d0aa3e2
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-05-18 11:27:55 -0500

    feat: enhance ToggleButton component with customizable dimensions and styles

--- apps/next-web/src/lib/components/ToggleButton.svelte
+++ apps/next-web/src/lib/components/ToggleButton.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
-  let { checked = $bindable(false) }: { checked: boolean } = $props();
+  let {
+    checked = $bindable(false),
+    width = "70px",
+    height = "34px",
+  }: {
+    checked: boolean;
+    width?: string;
+    height?: string;
+  } = $props();
 </script>
 
-<label class="toggle">
+<label
+  class="toggle"
+  style={`--toggle-width: ${width}; --toggle-height: ${height};`}
+>
   <input type="checkbox" id="btnToggle" name="btnToggle" bind:checked />
   <span class="slider"></span>
 </label>
 
 <style>
   .toggle {
+    --toggle-width: 90px;
+    --toggle-height: 34px;
+    --toggle-padding: 4px;
+    --thumb-size: calc(var(--toggle-height) - (var(--toggle-padding) * 2));
+
     position: relative;
     display: inline-block;
-    width: 60px;
-    height: 34px;
+    width: var(--toggle-width);
+    height: var(--toggle-height);
   }
 
   .toggle input {
@@ -28,16 +44,17 @@
     bottom: 0;
     background-color: #333;
     transition: 0.4s;
-    border-radius: 34px;
+    border-radius: var(--toggle-height);
+    overflow: hidden;
   }
 
   .slider:before {
     position: absolute;
     content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
+    height: var(--thumb-size);
+    width: var(--thumb-size);
+    left: var(--toggle-padding);
+    bottom: var(--toggle-padding);
     background-color: #fff;
     transition: 0.4s;
     border-radius: 50%;
@@ -48,6 +65,6 @@
   }
 
   input:checked + .slider:before {
-    transform: translateX(26px);
+    left: calc(100% - var(--thumb-size) - var(--toggle-padding));
   }
 </style>
